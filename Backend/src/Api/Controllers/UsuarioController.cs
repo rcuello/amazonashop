336026ec35809d540a782eb2cb1513feb1a8146b
Ronald Cuello <ronald.cuello@softwareone.com>
2025-05-15 13:22:52 -0500

    Add user management features in UsuarioController
    Updated UsuarioController with new endpoints for admin user management, including status updates and user retrieval by ID, token, and username. Introduced command and query classes for handling user updates and retrieval, along with pagination support. Implemented command handlers for user updates and added validation for commands. Created specification classes for user querying and counting. Enhanced overall user management capabilities for admin users.

--- Backend/src/Api/Controllers/UsuarioController.cs
+++ Backend/src/Api/Controllers/UsuarioController.cs
@@ -5,9 +5,18 @@ using Ecommerce.Application.Features.Auths.Users.Commands.RegisterUser;
 using Ecommerce.Application.Features.Auths.Users.Commands.ResetPassword;
 using Ecommerce.Application.Features.Auths.Users.Commands.ResetPasswordByToken;
 using Ecommerce.Application.Features.Auths.Users.Commands.SendPassword;
+using Ecommerce.Application.Features.Auths.Users.Commands.UpdateAdminStatusUser;
+using Ecommerce.Application.Features.Auths.Users.Commands.UpdateAdminUser;
 using Ecommerce.Application.Features.Auths.Users.Commands.UpdateUser;
+using Ecommerce.Application.Features.Auths.Users.Queries.GetUserById;
+using Ecommerce.Application.Features.Auths.Users.Queries.GetUserByToken;
+using Ecommerce.Application.Features.Auths.Users.Queries.GetUserByUsername;
+using Ecommerce.Application.Features.Auths.Users.Queries.PaginationUsers;
 using Ecommerce.Application.Features.Auths.Users.Vms;
+using Ecommerce.Application.Features.Shared.Queries;
+using Ecommerce.Application.Models.Authorization;
 using Ecommerce.Application.Models.ImageManagment;
+using Ecommerce.Domain;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -100,6 +109,65 @@ public class UsuarioController : ControllerBase
 
 
         return await _mediator.Send(request);
+    }
+
+    [Authorize(Roles = Role.ADMIN)]
+    [HttpPut("updateAdminUser", Name = "UpdateAdminUser")]
+    [ProducesResponseType(typeof(Usuario), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<Usuario>> UpdateAdminUser([FromBody] UpdateAdminUserCommand request)
+    {
+        return await _mediator.Send(request);
+    }
+
+
+    [Authorize(Roles = Role.ADMIN)]
+    [HttpPut("updateAdminStastusUser", Name = "UpdateAdminStastusUser")]
+    [ProducesResponseType(typeof(Usuario), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<Usuario>> UpdateAdminStastusUser([FromBody] UpdateAdminStatusUserCommand request)
+    {
+        return await _mediator.Send(request);
+    }
+
+
+    [Authorize(Roles = Role.ADMIN)]
+    [HttpGet("{id}", Name = "GetUsuarioById")]
+    [ProducesResponseType(typeof(AuthResponse), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<AuthResponse>> GetUsuarioById(string id)
+    {
+        var query = new GetUserByIdQuery(id);
+        return await _mediator.Send(query);
+    }
+
+
+
+    [HttpGet("", Name = "CurrentUser")]
+    [ProducesResponseType(typeof(AuthResponse), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<AuthResponse>> CurrentUser()
+    {
+        var query = new GetUserByTokenQuery();
+        return await _mediator.Send(query);
+    }
+
+
+    [Authorize(Roles = Role.ADMIN)]
+    [HttpGet("username/{username}", Name = "GetUsuarioByUsername")]
+    [ProducesResponseType(typeof(AuthResponse), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<AuthResponse>> GetUsuarioByUsername(string username)
+    {
+        var query = new GetUserByUsernameQuery(username);
+        return await _mediator.Send(query);
+    }
+
+
+    [Authorize(Roles = Role.ADMIN)]
+    [HttpGet("paginationAdmin", Name = "PaginationUser")]
+    [ProducesResponseType(typeof(PaginationVm<Usuario>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<PaginationVm<Usuario>>> PaginationUser(
+                    [FromQuery] PaginationUsersQuery paginationUsersQuery
+                )
+    {
+        var paginationUser = await _mediator.Send(paginationUsersQuery);
+        return Ok(paginationUser);
     }
 
 
