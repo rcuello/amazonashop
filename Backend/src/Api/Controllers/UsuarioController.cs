e637f0584381f0f8d75fb869979be4853abb2e3e
Ronald Cuello <ronald.cuello@softwareone.com>
2025-05-15 13:45:27 -0500

    Add new endpoints and restructure Ecommerce application
    - Introduced `GetRolesList` endpoint in `UsuarioController`.
    - Added `CategoryController` and `CountryController` for category and country retrieval.
    - Updated `Ecommerce.Application.csproj` to remove obsolete folder reference.
    - Renamed `Nombre` to `Name` in `Country.cs` for consistency.
    - Implemented query classes: `GetRolesQuery`, `GetCategoryListQuery`, and `GetCountryListQuery`.
    - Created view models `CategoryVm` and `CountryVm` for structured data representation.
    - Overall improvements enhance application functionality and code structure.

--- Backend/src/Api/Controllers/UsuarioController.cs
+++ Backend/src/Api/Controllers/UsuarioController.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using Ecommerce.Application.Contracts.Infrastructure;
+using Ecommerce.Application.Features.Auths.Queries.Roles.GetRoles;
 using Ecommerce.Application.Features.Auths.Users.Commands.LoginUser;
 using Ecommerce.Application.Features.Auths.Users.Commands.RegisterUser;
 using Ecommerce.Application.Features.Auths.Users.Commands.ResetPassword;
@@ -168,6 +169,16 @@ public class UsuarioController : ControllerBase
     {
         var paginationUser = await _mediator.Send(paginationUsersQuery);
         return Ok(paginationUser);
+    }
+
+    [AllowAnonymous]
+    [HttpGet("roles", Name = "GetRolesList")]
+    [ProducesResponseType(typeof(List<string>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<List<string>>> GetRolesList()
+    {
+        var query = new GetRolesQuery();
+        return Ok(await _mediator.Send(query));
+
     }
 
 
--- Backend/src/Core/Ecommerce.Domain/Country.cs
+++ Backend/src/Core/Ecommerce.Domain/Country.cs
@@ -4,7 +4,7 @@ namespace Ecommerce.Domain;
 
 public class Country : BaseDomainModel
 {    
-    public string? Nombre { get; set; }
+    public string? Name { get; set; }
     public string? Iso2{get;set;}
     public string? Iso3{get;set;}
 }
