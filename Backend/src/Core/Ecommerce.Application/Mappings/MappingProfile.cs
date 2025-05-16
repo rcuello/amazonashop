5621ba732187d1d1f5aa06841c84ef6df4dcf40c
Ronald Cuello <ronald.cuello@softwareone.com>
2025-05-16 16:54:21 -0500

    Add Stripe integration and order management features
    - Updated `appsettings.json` to include Stripe keys.
    - Enhanced `MappingProfile` with new mappings for address and order models.
    - Introduced `CreateAddressCommand` and handler for address creation.
    - Added `CreateAddressCommandValidator` for input validation.
    - Created `CreateOrderCommand` and handler for order creation, including payment processing with Stripe.
    - Implemented `UpdateOrderCommand` and handler for updating order status.
    - Added `GetOrdersByIdQuery` and handler for retrieving orders by ID.
    - Created view models `OrderItemVm` and `OrderVm` for order representation.
    - Added `OrderStatusLabel` for order status constants.
    - Introduced `StripeSettings` class for managing Stripe configuration.

--- Backend/src/Core/Ecommerce.Application/Mappings/MappingProfile.cs
+++ Backend/src/Core/Ecommerce.Application/Mappings/MappingProfile.cs
@@ -1,7 +1,9 @@
 using AutoMapper;
+using Ecommerce.Application.Features.Addresses.Vms;
 using Ecommerce.Application.Features.Categories.Vms;
 using Ecommerce.Application.Features.Countries.Vms;
 using Ecommerce.Application.Features.Images.Queries.Vms;
+using Ecommerce.Application.Features.Orders.Vms;
 using Ecommerce.Application.Features.Products.Commands.CreateProduct;
 using Ecommerce.Application.Features.Products.Commands.UpdateProduct;
 using Ecommerce.Application.Features.Products.Queries.Vms;
@@ -30,7 +32,12 @@ public class MappingProfile : Profile
                 .ForMember(p => p.ShoppingCartId, x => x.MapFrom(a => a.ShoppingCartMasterId));
 
         CreateMap<ShoppingCartItem, ShoppingCartItemVm>();
-
         CreateMap<ShoppingCartItemVm, ShoppingCartItem>();
+
+        CreateMap<Address, AddressVm>();
+
+        CreateMap<Order, OrderVm>();
+        CreateMap<OrderItem, OrderItemVm>();
+        CreateMap<OrderAddress, AddressVm>();
     }
 }
