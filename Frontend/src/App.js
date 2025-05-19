d7ad39a8a11b4f11b2eee90acc3d3680e9f73b54
Ronald Cuello <ronald.cuello@softwareone.com>
2025-05-19 14:54:39 -0500

    feat: add Home component with product display to the application

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './components/Home';
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Home/>
       <Footer />
     </div>
   );
